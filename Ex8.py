13417234bd7fccfc11025da3f17cdfe91b550f86
Sasha Latypova <sashalat42@gmail.com>
2022-10-24 22:18:14 +0300

    new version Ex8.py

--- Ex8.py
+++ Ex8.py
@@ -1,14 +1,15 @@
 import requests
 import time
-import json
 
 response = requests.get('https://playground.learnqa.ru/ajax/api/longtime_job')
-answer = json.loads(response.text)
+answer = response.json()
 
 payload = {'token': answer["token"]}
 before = requests.get('https://playground.learnqa.ru/ajax/api/longtime_job', params=payload)
-print(before.text)
+not_ready = before.json()
+print(not_ready['status'])
 
 time.sleep(answer["seconds"])
 after = requests.get('https://playground.learnqa.ru/ajax/api/longtime_job', params=payload)
-print(after.text)
+ready = after.json()
+print(f"{ready['status']}! Your result is {ready['result']}")
